07d5f33f78305cca0d172880113f3909e98efa49
Adrian-Lugowski <adrian.lugowski.elektryk@gmail.com>
2026-05-06 20:49:36 +0200

    lab_11

---
+++ lab_09/bullet.gd
@@ -0,0 +1,12 @@
+extends Area3D
+
+@export var speed: float = 30.0
+@export var lifetime: float = 3.0
+@export var direction: Vector3 = Vector3.ZERO
+
+func _process(delta):
+	position += direction * speed * delta
+
+	lifetime -= delta
+	if lifetime <= 0:
+		queue_free()
