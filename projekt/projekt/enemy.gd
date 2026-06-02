c44f4371d887e8e9e3a683cffbc8c7bda17fc784
Adrian Ługowski <adrian.lugowski.elektryk@gmail.com>
2026-06-02 18:11:17 +0200

    enemy ai

--- projekt/projekt/enemy.gd
+++ projekt/projekt/enemy.gd
@@ -1,18 +1,33 @@
 extends Area2D
 
 var hp = 2
+const TILE_SIZE =64
 
 func _ready():
-	position = position.snapped(Vector2(64, 64))
+	position = position.snapped(Vector2(TILE_SIZE, TILE_SIZE))
 
 func take_damage(amaount):
 	hp -= amaount
 	print("Trawiony, zostało:", hp, "HP")
 	
 	if hp <= 0:
-		get_tree().change_scene_to_file("res://win.tscn")
-		queue_free()
+		get_tree().call_deferred("change_scene_to_file", "res://lab_15/win.tscn")
+		call_deferred("queue_free")
 
 func _on_area_entered(area):
 	if area.name == "Player":
 		get_tree().call_deferred("change_scene_to_file", "res://game_over.tscn")
+		
+#func _on_rhythm_timer_timeout():
+	var player = get_tree().get_first_node_in_group("player")
+	
+	if player:
+		var direction_to_player = global_position.direction_to(player.global_position)
+		var step = Vector2.ZERO
+		
+		if abs(direction_to_player.x) > abs(direction_to_player.y):
+			step.x = sign(direction_to_player.x)
+		else:
+			step.y = sign(direction_to_player.y)
+		
+		position += step * TILE_SIZE
