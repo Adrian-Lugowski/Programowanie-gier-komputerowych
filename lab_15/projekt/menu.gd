2738efa16fb0503970da87d0a558d649d2a67b91
Adrian Ługowski <adrian.lugowski.elektryk@gmail.com>
2026-06-06 12:35:38 +0200

    final

--- lab_15/projekt/menu.gd
+++ lab_15/projekt/menu.gd
@@ -2,3 +2,15 @@ extends Control
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
+
+func _ready():
+	var title = $Label 
+	title.pivot_offset = title.size / 2 
+	
+	var tween = create_tween().set_loops()
+	tween.tween_property(title, "scale", Vector2(1.1, 1.1), 0.5).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(title, "scale", Vector2(1.0, 1.0), 0.5).set_trans(Tween.TRANS_SINE)
+
+
+func _on_button_mouse_entered():
+	$AudioStreamPlayer2D.play()
